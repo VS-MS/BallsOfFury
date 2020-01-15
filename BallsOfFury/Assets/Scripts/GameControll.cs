8dff5b1f738d47c09c1dccf8edb12026c6a266a5
Dmitry Vlasov <vantusmantus@gmail.com>
2020-01-15 10:25:06 +0500

    First load
    First load

---
+++ BallsOfFury/Assets/Scripts/GameControll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControll : MonoBehaviour
+{
+    public static GameControll instance { get; private set; }
+
+    void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        //Проверяем, есть ли экземпляр объекта на сцене, если есть, удаляем этот экземпляр
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            //return;
+        }
+    }
+
+    public void SetPauseOn()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void SetPauseOff()
+    {
+        Time.timeScale = 1;
+    }
+}
